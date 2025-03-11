f971c71234dded4b0e29f0662793b7f00cb594b0
Saurav Karki <sauravkarki10.12@gmail.com>
2025-03-11 23:39:14 +0545

    updated

---
+++ src/app/clients/add-client/page.tsx
@@ -0,0 +1,12 @@
+import AddClient from "@/components/clients/AddClient"
+import React from "react"
+
+const page = () => {
+  return (
+    <div>
+      <AddClient />
+    </div>
+  )
+}
+
+export default page
